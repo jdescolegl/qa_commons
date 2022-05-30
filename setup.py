56dba3d5f45a6d45004246f007b60aeff354609b
jdescolegl <jeronimo.descole@globallogic.com>
2022-05-30 15:10:30 -0300

    a change in setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-with pathlib.Path('requirements.txt').open() as requirements_txt:
+with pathlib.Path('./requirements.txt').open() as requirements_txt:
     install_requires = [
         str(requirement)
         for requirement
